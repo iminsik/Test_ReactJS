15273630fbea091096778701c49dca23cc66b853
Insik Cho <insik.cho@alaskaair.com>
2016-09-05 14:33:35 -0700

    refactoring

--- public/app.jsx
+++ public/app.jsx
@@ -1,39 +1,6 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
-var GreeterMessage = require('./components/GreeterMessage');
-var GreeterForm = require('./components/GreeterForm');
-
-
-var Greeter = React.createClass({
-  getDefaultProps: function() {
-    return {
-      name: 'React',
-      message: 'This is a message'
-    }
-  },
-  getInitialState: function() {
-    return {
-      name: this.props.name,
-      message: this.props.message
-    }
-  },
-  handleNewName: function(name) {
-      this.setState({name: name});
-  },
-  handleNewMessage: function(message) {
-      this.setState({message: message});
-  },
-  render: function() {
-    var name = this.state.name;
-    var message = this.state.message;
-    return (
-      <div>
-        <GreeterMessage name={name} message={message}/>
-        <GreeterForm onNewName={this.handleNewName} onNewMessage={this.handleNewMessage}/>
-      </div>
-    )
-  }
-});
+const Greeter = require('./components/Greeter')
 
 ReactDOM.render(
   <Greeter />,
