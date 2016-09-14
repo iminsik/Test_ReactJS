8fb6710d7dcdd637e8849337da192205cf2ef309
Insik Cho <insik.cho@alaskaair.com>
2016-09-13 22:07:05 -0700

    refactor weatherMessage
    with es6 destructuring syntax

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,7 +1,6 @@
 const React = require('react');
 
-module.exports = (props) => {
-    var {temp, location} = props;
+module.exports = ({temp, location}) => {
     return (
       <div>
         <h3>It's it {temp} in {location}.</h3>
