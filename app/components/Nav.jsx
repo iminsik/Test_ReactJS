32c3f0148d665322a6e5845bcbb00a3e3582e539
Insik Cho <insik.cho@alaskaair.com>
2016-09-15 13:14:31 -0700

    some more dots

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -5,7 +5,7 @@ module.exports = (props) =>
 (
   <div>
     <h2>Nav Component</h2>
-    <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Weather</IndexLink>
+    <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Weather...</IndexLink>
     <Link to="/About" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
     <Link to="/Example" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Example</Link>
 
