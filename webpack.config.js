0078aa4fc1f96eaddf8a038ade6c748fdb151508
Insik Cho <insik.cho@alaskaair.com>
2016-10-05 21:29:59 -0700

    apply sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
       OpenWeatherMap: 'app/api/openWeatherMap.jsx',
       WeatherMessage: 'app/components/WeatherMessage.jsx',
       ErrorModal: 'app/components/ErrorModal.jsx',
-      applicationStyles: 'app/styles/app.css'
+      applicationStyles: 'app/styles/app.scss'
     },
     extensions: ['', '.js', '.jsx']
   },
